467ab1ae815fcf6456b915a75470bd4f65b366e4
AdrianaPugarito <85944220+AdrianaPugarito@users.noreply.github.com>
2021-08-20 02:28:51 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+//SAIDA DE DADOS
+
+//innerHTML [interagir com os elementos html]
+document.getElementById("teste1").innerHTML = "Adriana esteve aqui"
+
+//innerText [So altera o valor do text no content]
+document.getElementById("teste2").innerText = "<h3>Adriana 2</h3>"
+
+// innerHTML conseguimos formatar o elemento
+document.getElementById("teste3").innerHTML = "<h3 style='color:blue;'> Este texto esta formatado</h3>"
+
+console.log(1)
+
+document.write("Documento write")
+
+window.alert("SPAM SPAM SPAM SPAM")
